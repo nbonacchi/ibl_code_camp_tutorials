e1a793176b66f7a00a55a99970a9b85b15568150
zashwood <zashwood@princeton.edu>
2019-09-02 17:42:47 -0400

    Corrected spelling mistake in comment

--- my_module.py
+++ my_module.py
@@ -1,3 +1,3 @@
 print('Hello World!')
 print("Here's something interesting:\n")
-# Add lines here, let's make this script do something ineterting!! 
+# Add lines here, let's make this script do something interesting!! 
